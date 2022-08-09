7a2a824c6950269fd93f88e41c23a9d5b8987dce
Robson <robccorreia@gmail.com>
2022-08-09 17:38:10 -0300

    feat: render list with  mustache

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,36 @@
 import { Router, Request, Response } from "express";
 
 const router = Router();
+
+interface Product {
+  title: string;
+  price: number;
+}
 interface User {
   name: string;
   age: number;
+  status: boolean;
+  address: string[];
+  products: Product[];
 }
 
 router.get("/", (req: Request, res: Response) => {
+  const activeUser: boolean = true;
+  let status: boolean = false;
+  if (activeUser) {
+    status = true;
+  }
+
   const user: User = {
     name: "Robson",
     age: 27,
+    status,
+    address: ["Rua 1", "Rua 2", "Rua 3"],
+    products: [
+      { title: "Produto x", price: 10 },
+      { title: "Produto y", price: 15 },
+      { title: "Produto w", price: 20 },
+    ],
   };
   res.render("home", {
     user,

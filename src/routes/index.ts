76f302cf7d0eb94b11f369baad7d53950ee41ad0
Robson <robccorreia@gmail.com>
2022-08-09 11:43:06 -0300

    feat: separe router in a paste with mainRouter

---
+++ src/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router, Request, Response } from 'express'
+
+const router = Router()
+
+router.get('/', (req: Request, res: Response) => {
+  res.send('Home');
+})
+
+router.get('/sobre', (req: Request, res: Response) => {
+  res.send('Sobre');
+})
+
+router.get('/contato', (req: Request, res: Response) => {
+  res.send('Contato');
+})
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,8 @@
-import express, { Request, Response} from 'express'
+import express from "express";
+import mainRoutes from "./routes/index";
 
-const app = express()
+const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello')
-})
+app.use(mainRoutes);
 
-app.get('/noticia/:slug', (req: Request, res: Response) => {
-  const slug:string = req.params.slug;
-  res.send(`Notícia: ${slug}`)
-})
-
-app.get('/voo/:origem-:destino', (req: Request, res: Response) => {
-  const origem = req.params.origem;
-  const destino = req.params.destino
-  res.send(`Voo de ${origem.toUpperCase()} para ${destino.toUpperCase()}`)
-})
-
-// site.com/voo/gru-rec
-
-app.listen(3000)
-
+app.listen(3000);

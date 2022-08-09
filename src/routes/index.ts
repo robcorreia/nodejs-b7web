b515c9738905232ce2b32e8fc53dee52edf8e7ad
Robson <robccorreia@gmail.com>
2022-08-09 16:43:57 -0300

    feat: add and configure mustache-express

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("Home");
+  res.render("home");
 });
 
 router.get("/sobre", (req: Request, res: Response) => {
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from "express";
 import path from "path";
+import mustache from "mustache-express";
 import mainRoutes from "./routes/index";
 
 const app = express();
+
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "views"));
+app.engine("mustache", mustache());
 
 app.use("/static", express.static(path.join(__dirname, "../public")));
 

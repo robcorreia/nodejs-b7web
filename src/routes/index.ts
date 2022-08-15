439951ceea198c67273d6a290d079d6bb6dc14a3
Robson <robccorreia@gmail.com>
2022-08-15 11:16:50 -0300

    feat: get params from url

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -45,4 +45,29 @@ router.get("/contato", (req: Request, res: Response) => {
   res.render("pages/contact");
 });
 
+router.get('/nome', (req: Request, res: Response) => {
+  let nome: string = req.query.nome as string;
+  let idade: string = req.query.idade as string;
+  res.render('pages/nome', {
+    nome,
+    idade
+  })
+})
+
+router.get('/idade', (req: Request, res: Response) => {
+  let mostrarIdade: boolean = false;
+  let idade: number = 0;
+  if (req.query.ano) {
+    let anoNascimento: number = parseInt(req.query.ano as string);
+    let anoAtual: number = new Date().getFullYear();
+    idade = anoAtual - anoNascimento;
+    mostrarIdade = true;
+
+  }
+  res.render('pages/idade', {
+    idade,
+    mostrarIdade
+  });
+})
+
 export default router;
